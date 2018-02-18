26b0656c417d09b5197eff864e7936f27478bf53
squinkylabs <squinkylabs@users.noreply.github.com>
2018-02-17 23:03:56 -0800

    cleanup

--- src/BootyModule.cpp
+++ src/BootyModule.cpp
@@ -9,51 +9,56 @@
 /**
  * Implementation class for BootyWidget
  */
-struct BootyModule : Module {
-    enum ParamIds {
-		PITCH_PARAM,      // the big pitch knob
-		NUM_PARAMS
-	};
-    
-	enum InputIds {
-		AUDIO_INPUT,
+struct BootyModule : Module
+{
+    enum ParamIds
+    {
+        PITCH_PARAM,      // the big pitch knob
+        NUM_PARAMS
+    };
+
+    enum InputIds
+    {
+        AUDIO_INPUT,
         CV_INPUT,
-		NUM_INPUTS
-	};
-    
-	enum OutputIds {
-		SIN_OUTPUT,
+        NUM_INPUTS
+    };
+
+    enum OutputIds
+    {
+        SIN_OUTPUT,
         COS_OUTPUT,
-		NUM_OUTPUTS
-	};
-    
-   enum LightIds { 
-		NUM_LIGHTS
-	};
+        NUM_OUTPUTS
+    };
+
+    enum LightIds
+    {
+        NUM_LIGHTS
+    };
 
     BootyModule();
-    
+
     /**
      * Overrides of Module functions
      */
-	void step() override;
+    void step() override;
     json_t *toJson() override;
-	void fromJson(json_t *rootJ) override;
+    void fromJson(json_t *rootJ) override;
     void onSampleRateChange() override;
- 
-private: 
+
+private:
     typedef float T;
     SinOscillatorParams<T> oscParams;
-	SinOscillatorState<T> oscState;
+    SinOscillatorState<T> oscState;
     BiquadParams<T, 3> hilbertFilterParamsSin;
-	BiquadParams<T, 3> hilbertFilterParamsCos;
+    BiquadParams<T, 3> hilbertFilterParamsCos;
     BiquadState<T, 3> hilbertFilterStateSin;
-	BiquadState<T, 3> hilbertFilterStateCos;
-    
+    BiquadState<T, 3> hilbertFilterStateCos;
+
     LookupTableParams<T> exponential;
-    
+
     float reciprocolSampleRate;
-    public:
+public:
     float freqRange = 5;
     ChoiceButton *rangeChoice;
 };
@@ -61,43 +66,46 @@ private:
 extern float values[];
 extern const char* ranges[];
 
-BootyModule::BootyModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
-    
-    // TODO: can we assume onSampleRateChange() gets called first, so this is unneccessary?
+BootyModule::BootyModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS)
+{
+    // TODO: can we assume onSampleRateChange() gets called first, so this is unnecessary?
     onSampleRateChange();
-    
+
     // Force lazy load of lookup table with this extra call
     SinOscillator<T, true>::setFrequency(oscParams, .01);
-    
+
     // Make 128 entry table to do exp x={-5..5} y={2..2000}
     std::function<double(double)> expFunc = AudioMath::makeFunc_Exp(-5, 5, 2, 2000);
     LookupTable<T>::init(exponential, 128, -5, 5, expFunc);
 }
 
-void BootyModule::onSampleRateChange() {
+void BootyModule::onSampleRateChange()
+{
     T rate = engineGetSampleRate();
-    reciprocolSampleRate = 1/rate;
+    reciprocolSampleRate = 1 / rate;
     HilbertFilterDesigner<T>::design(rate, hilbertFilterParamsSin, hilbertFilterParamsCos);
 }
 
-json_t *BootyModule::toJson() {
+json_t *BootyModule::toJson()
+{
     json_t *rootJ = json_object();
     int rg = freqRange;
     json_object_set_new(rootJ, "range", json_integer(rg));
     return rootJ;
 }
 
-void BootyModule::fromJson(json_t *rootJ)  {
+void BootyModule::fromJson(json_t *rootJ)
+{
 
     json_t *driverJ = json_object_get(rootJ, "range");
     if (driverJ) {
         const int rg = json_number_value(driverJ);
-        
+
         // TODO: should we be more robust about float <> int issues?
         //need to tell the control what text to display
-        for (int i=0; i<5; ++i) {
+        for (int i = 0; i < 5; ++i) {
             if (rg == values[i]) {
-               rangeChoice->text = ranges[i]; 
+                rangeChoice->text = ranges[i];
             }
         }
         freqRange = rg;
@@ -106,7 +114,8 @@ void BootyModule::fromJson(json_t *rootJ)  {
 }
 
 
-void BootyModule::step() {
+void BootyModule::step()
+{
     // add the knob and the CV
     T freqHz;
     T cvTotal = params[PITCH_PARAM].value + inputs[CV_INPUT].value;
@@ -125,20 +134,20 @@ void BootyModule::step() {
     }
 
     SinOscillator<float, true>::setFrequency(oscParams, freqHz * reciprocolSampleRate);
-    
+
     // Generate the quadrature sin oscillators.
     T x, y;
     SinOscillator<T, true>::runQuadrature(x, y, oscState, oscParams);
-   
+
     // Filter the input through th quadrature filter
     const T input = inputs[AUDIO_INPUT].value;
     const T hilbertSin = BiquadFilter<T>::run(input, hilbertFilterStateSin, hilbertFilterParamsSin);
     const T hilbertCos = BiquadFilter<T>::run(input, hilbertFilterStateCos, hilbertFilterParamsCos);
-    
+
     // Cross modulate the two sections.
     x *= hilbertSin;
     y *= hilbertCos;
-   
+
     // And combine for final SSB output.
     outputs[SIN_OUTPUT].value = x + y;
     outputs[COS_OUTPUT].value = x - y;
@@ -151,10 +160,10 @@ void BootyModule::step() {
  ***********************************************************************************/
 
 const char* ranges[5] = {
-    "5 hz",
-    "50 hz",
-    "500 hz",
-    "5 khz",
+    "5 Hz",
+    "50 Hz",
+    "500 Hz",
+    "5 kHz",
     "exp"
 };
 
@@ -166,44 +175,50 @@ float values[5] = {
     0
 };
 
-struct RangeItem : MenuItem {
+struct RangeItem : MenuItem
+{
     RangeItem(int index, float * output, ChoiceButton * inParent) :
-        rangeIndex(index), rangeOut(output), rangeChoice( inParent) {
-            text = ranges[index];
+        rangeIndex(index), rangeOut(output), rangeChoice(inParent)
+    {
+        text = ranges[index];
     }
 
     const int rangeIndex;
     float * const rangeOut;
     ChoiceButton* const rangeChoice;
- 
-    void onAction(EventAction &e) override {
+
+    void onAction(EventAction &e) override
+    {
         rangeChoice->text = ranges[rangeIndex];
         *rangeOut = values[rangeIndex];
     }
 };
 
-struct RangeChoice : ChoiceButton {
-  
-    RangeChoice(float * out, const Vec& pos, float width) : output(out) {
+struct RangeChoice : ChoiceButton
+{
+
+    RangeChoice(float * out, const Vec& pos, float width) : output(out)
+    {
         assert(*output == 5);
         this->text = std::string(ranges[0]);
         this->box.pos = pos;
         this->box.size.x = width;
     }
     float * const output;
-	void onAction(EventAction &e) override {
-		Menu *menu = gScene->createMenu();
+    void onAction(EventAction &e) override
+    {
+        Menu *menu = gScene->createMenu();
 
         menu->box.pos = getAbsoluteOffset(Vec(0, box.size.y)).round();
-		menu->box.size.x = box.size.x;    
-		{
-            menu->addChild( new RangeItem(0, output, this));
-            menu->addChild( new RangeItem(1, output, this));
-            menu->addChild( new RangeItem(2, output, this));
-            menu->addChild( new RangeItem(3, output, this));
-            menu->addChild( new RangeItem(4, output, this));
-		}
-	}
+        menu->box.size.x = box.size.x;
+        {
+            menu->addChild(new RangeItem(0, output, this));
+            menu->addChild(new RangeItem(1, output, this));
+            menu->addChild(new RangeItem(2, output, this));
+            menu->addChild(new RangeItem(3, output, this));
+            menu->addChild(new RangeItem(4, output, this));
+        }
+    }
 };
 
 ////////////////////
@@ -211,75 +226,72 @@ struct RangeChoice : ChoiceButton {
 ////////////////////
 
 
-struct BootyWidget : ModuleWidget {
-	BootyWidget(BootyModule *);
+struct BootyWidget : ModuleWidget
+{
+    BootyWidget(BootyModule *);
 };
 
 /**
- * Widget constructor will describe my implementation struct and
- * provide metadata.
- * This is not shared by all modules in the dll, just one
+ * Widget constructor will describe my implementation structure and
+ * provide meta-data.
+ * This is not shared by all modules in the DLL, just one
  */
-BootyWidget::BootyWidget(BootyModule *module) : ModuleWidget(module) {
-   // BootyModule *module = new BootyModule();
-//	setModule(module);
-	box.size = Vec(6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+BootyWidget::BootyWidget(BootyModule *module) : ModuleWidget(module)
+{
+    // BootyModule *module = new BootyModule();
+ //	setModule(module);
+    box.size = Vec(6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
 
-	{
-		SVGPanel *panel = new SVGPanel();
-		panel->box.size = box.size;
-		panel->setBackground(SVG::load(assetPlugin(plugin, "res/booty_panel.svg")));
-		addChild(panel);
-	}
-    
+    {
+        SVGPanel *panel = new SVGPanel();
+        panel->box.size = box.size;
+        panel->setBackground(SVG::load(assetPlugin(plugin, "res/booty_panel.svg")));
+        addChild(panel);
+    }
+
     const int leftInputX = 11;
     const int rightInputX = 55;
-    
+
     const int row0 = 45;
     const int row1 = 102;
     static int row2 = 186;
-   
+
     // Inputs on Row 0
-    // addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, MyModule::PITCH_INPUT));
     addInput(Port::create<PJ301MPort>(Vec(leftInputX, row0), Port::INPUT, module, BootyModule::AUDIO_INPUT));
     addInput(Port::create<PJ301MPort>(Vec(rightInputX, row0), Port::INPUT, module, BootyModule::CV_INPUT));
-    
+
     // shift Range on row 2
-    const float margin = 16; 
+    const float margin = 16;
     float xPos = margin;
-	float width =  6 * RACK_GRID_WIDTH - 2 * margin; 
-   
-    module->rangeChoice = new RangeChoice(&module->freqRange, Vec(xPos, row2), width); 
+    float width = 6 * RACK_GRID_WIDTH - 2 * margin;
+
+    module->rangeChoice = new RangeChoice(&module->freqRange, Vec(xPos, row2), width);
     addChild(module->rangeChoice);
 
-
     // knob on row 1
-    //addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, MyModule::PITCH_PARAM, -3.0, 3.0, 0.0));
     addParam(ParamWidget::create<Rogan3PSBlue>(Vec(18, row1), module, BootyModule::PITCH_PARAM, -5.0, 5.0, 0.0));
 
     const float row3 = 317.5;
-    
+
     // Outputs on row 3
     const float leftOutputX = 9.5;
     const float rightOutputX = 55.5;
-    //addOutput(Port::create<PJ301MPort>(Vec(33, 275), Port::OUTPUT, module, MyModule::SINE_OUTPUT));
-	addOutput(Port::create<PJ301MPort>(Vec(leftOutputX, row3), Port::OUTPUT, module, BootyModule::SIN_OUTPUT));
+
+    addOutput(Port::create<PJ301MPort>(Vec(leftOutputX, row3), Port::OUTPUT, module, BootyModule::SIN_OUTPUT));
     addOutput(Port::create<PJ301MPort>(Vec(rightOutputX, row3), Port::OUTPUT, module, BootyModule::COS_OUTPUT));
-    
+
     // screws
     addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-	addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 }
-
-
 
 // Specify the Module and ModuleWidget subclass, human-readable
 // manufacturer name for categorization, module slug (should never
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
 Model *modelBootyModule = Model::create<BootyModule, BootyWidget>("Squinky Labs",
-                                                                  "squinkylabs-freqshifter",
-                                                                  "Booty Frequency Shifter", EFFECT_TAG , RING_MODULATOR_TAG);
+    "squinkylabs-freqshifter",
+    "Booty Frequency Shifter", EFFECT_TAG, RING_MODULATOR_TAG);
 
--- src/Squinky.hpp
+++ src/Squinky.hpp
@@ -1,21 +1,5 @@
 #include "rack.hpp"
 
-
 using namespace rack;
-
-
 extern Plugin *plugin;
-
 extern Model *modelBootyModule;
-
-#if 0
-////////////////////
-// module widgets
-////////////////////
-
-// first module widget is simple freq shifter.
-struct BootyWidget : ModuleWidget {
-	BootyWidget(BootyModule *);
-};
-
-#endif
