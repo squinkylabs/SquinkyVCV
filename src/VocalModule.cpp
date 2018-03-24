028197ff1bc6f6161600e37750d911e0c7d94aa9
squinkylabs <squinkylabs@users.noreply.github.com>
2018-03-23 19:54:40 -0700

    new ui

--- src/VocalModule.cpp
+++ src/VocalModule.cpp
@@ -108,7 +108,7 @@ VocalWidget::VocalWidget(VocalModule *module) : ModuleWidget(module)
 
     Label *label = new Label();
     label->box.pos = Vec(lfoLabelX, lfoLabelY);
-    label->text = "Rate";
+    label->text = "LFO";
     label->color = COLOR_BLACK;
     addChild(label);  
     addParam(ParamWidget::create<Rogan1PSBlue>(
@@ -121,6 +121,7 @@ VocalWidget::VocalWidget(VocalModule *module) : ModuleWidget(module)
     /**
      * Parameters and CV
      */ 
+    /*
     const float knobX = 75;
     const float trimX = 46;
     const float inputX = 8;
@@ -133,85 +134,125 @@ VocalWidget::VocalWidget(VocalModule *module) : ModuleWidget(module)
     
 
     const float matrixY = knobY + 40;
+    */
 
-    knobY += space;
+    const float mainBlockY = 140;
+    const float mainBlockX = 28;
+
+    const float colSpacingX = 60;
+
+    const float knobX = mainBlockX+0;
+    const float knobY = mainBlockY+0;
+
+    const float trimX = mainBlockX+11;
+    const float trimY = mainBlockY + 50;
+
+    const float labelX = mainBlockX+0;
+    const float labelY = mainBlockY+80;
+
+    const float inputX = mainBlockX+5;
+    const float inputY = mainBlockY+100;
+
     label = new Label();
-    label->box.pos = Vec(labelX, knobY+labelOffset);
+    label->box.pos = Vec(labelX, labelY);
+    label->text = "Fc";
+    label->color = COLOR_BLACK;
+    addChild(label);
+    addParam(ParamWidget::create<Rogan1PSBlue>(
+        Vec(knobX, knobY), module, module->animator.FILTER_FC_PARAM, -5.0, 5.0, 0.0));
+    addInput(Port::create<PJ301MPort>(
+        Vec(inputX, inputY), Port::INPUT, module, VocalModule::Animator::FILTER_FC_CV_INPUT));
+    addParam(ParamWidget::create<Trimpot>(
+        Vec(trimX, trimY), module, module->animator.FILTER_FC_TRIM_PARAM, -1.0, 1.0, 1.0));
+      
+
+   
+    label = new Label();
+    label->box.pos = Vec(labelX + colSpacingX + 2, labelY);
     label->text = "Q";
     label->color = COLOR_BLACK;
     addChild(label);
-    addParam(ParamWidget::create<Rogan1PSBlue>(Vec(knobX, knobY), module, module->animator.FILTER_Q_PARAM, -5.0, 5.0, 0.0));
-    addInput(Port::create<PJ301MPort>(Vec(inputX, knobY+inputYOffset), Port::INPUT, module, VocalModule::Animator::FILTER_Q_CV_INPUT));
-    addParam(ParamWidget::create<Trimpot>(Vec(trimX, knobY+trimYOffset), module, module->animator.FILTER_Q_TRIM_PARAM, -1.0, 1.0, 1.0));
+
+    addParam(ParamWidget::create<Rogan1PSBlue>(
+        Vec(knobX + colSpacingX, knobY), module, module->animator.FILTER_Q_PARAM, -5.0, 5.0, 0.0));
+    addInput(Port::create<PJ301MPort>(
+        Vec(inputX + colSpacingX, inputY), Port::INPUT, module, VocalModule::Animator::FILTER_Q_CV_INPUT));
+    addParam(ParamWidget::create<Trimpot>(
+        Vec(trimX + colSpacingX, trimY), module, module->animator.FILTER_Q_TRIM_PARAM, -1.0, 1.0, 1.0));
    
-    knobY += space;
     label = new Label();
-    label->box.pos = Vec(labelX, knobY+labelOffset);
-    label->text = "Fc filter";
+    label->box.pos = Vec(labelX + 2*colSpacingX - 6, labelY);
+    label->text = "Depth";
     label->color = COLOR_BLACK;
     addChild(label);
-    addParam(ParamWidget::create<Rogan1PSBlue>(Vec(knobX, knobY), module, module->animator.FILTER_FC_PARAM, -5.0, 5.0, 0.0));
-    addInput(Port::create<PJ301MPort>(Vec(inputX, knobY+inputYOffset), Port::INPUT, module, VocalModule::Animator::FILTER_FC_CV_INPUT));
-    addParam(ParamWidget::create<Trimpot>(Vec(trimX, knobY+trimYOffset), module, module->animator.FILTER_FC_TRIM_PARAM, -1.0, 1.0, 1.0));
-      
-    knobY += space;
-    label = new Label();
-    label->box.pos = Vec(labelX, knobY+labelOffset);
-    label->text = "Mod Depth";
-    label->color = COLOR_BLACK;
-    addChild(label);
-    addParam(ParamWidget::create<Rogan1PSBlue>(Vec(knobX, knobY), module, module->animator.FILTER_MOD_DEPTH_PARAM, -5.0, 5.0, 0.0));
-    addInput(Port::create<PJ301MPort>(Vec(inputX, knobY+inputYOffset), Port::INPUT, module, VocalModule::Animator::FILTER_MOD_DEPTH_CV_INPUT));
-    addParam(ParamWidget::create<Trimpot>(Vec(trimX, knobY+trimYOffset), module, module->animator.FILTER_MOD_DEPTH_TRIM_PARAM, -1.0, 1.0, 1.0));
+    addParam(ParamWidget::create<Rogan1PSBlue>(
+        Vec(knobX + 2*colSpacingX, knobY), module, module->animator.FILTER_MOD_DEPTH_PARAM, -5.0, 5.0, 0.0));
+    addInput(Port::create<PJ301MPort>(
+        Vec(inputX + 2*colSpacingX, inputY), Port::INPUT, module, VocalModule::Animator::FILTER_MOD_DEPTH_CV_INPUT));
+    addParam(ParamWidget::create<Trimpot>(
+        Vec(trimX + 2*colSpacingX, trimY), module, module->animator.FILTER_MOD_DEPTH_TRIM_PARAM, -1.0, 1.0, 1.0));
   
     const float row3 = 317.5;
 
     // I.O on row 3
-    const float AudioInputX = 10.0;
-    const float outputX = 57.0;
+    const float AudioInputX = 28.0;
+    const float outputX = 80.0;
  
-    addInput(Port::create<PJ301MPort>(Vec(AudioInputX, row3), Port::INPUT, module, VocalModule::Animator::AUDIO_INPUT));
-    addOutput(Port::create<PJ301MPort>(Vec(outputX, row3), Port::OUTPUT, module, VocalModule::Animator::AUDIO_OUTPUT));
+    addInput(Port::create<PJ301MPort>(
+        Vec(AudioInputX, row3), Port::INPUT, module, VocalModule::Animator::AUDIO_INPUT));
+    addOutput(Port::create<PJ301MPort>(
+        Vec(outputX, row3), Port::OUTPUT, module, VocalModule::Animator::AUDIO_OUTPUT));
     
     label = new Label();
-    label->box.pos = Vec(AudioInputX, row3 - 20);
+    label->box.pos = Vec(AudioInputX-2, row3 - 20);
     label->text = "In";
     label->color = COLOR_BLACK;
     addChild(label);  
     label = new Label();
-    label->box.pos = Vec(outputX, row3 - 20);
+    label->box.pos = Vec(outputX-6, row3 - 20);
     label->text = "Out";
     label->color = COLOR_BLACK;
     addChild(label);  
     
+
+
     /*******************************************
      *  temp test switches
      */
-    const float switchX = width-20; 
+    const float switchY = 306;
+    const float switchX = 124;
+    const float switchSpacingX = 22;
+    const float switchLabelY=switchY - 20;
+
+     // 3 pos LFO matrix
+    addParam(ParamWidget::create<CKSSThree>(
+        Vec(switchX,switchY), module, module->animator.LFO_MIX_PARAM, 0.0f, 2.0f, 0.0f));
+    label = new Label();
+    label->box.pos = Vec(switchX -15, switchLabelY);
+    label->text = "Mtx";
+    label->color = COLOR_BLACK;
+    addChild(label);  
          
     // 2 pos bass_exp
-    addParam(ParamWidget::create<CKSS>( Vec(switchX, 205), module, module->animator.BASS_EXP_PARAM, 0.0f, 1.0f, 0.0f));     
+    addParam(ParamWidget::create<CKSS>(
+        Vec(switchX + switchSpacingX, switchY), module, module->animator.BASS_EXP_PARAM, 0.0f, 1.0f, 0.0f));     
     label = new Label();
-    label->box.pos = Vec(switchX -8, 225);
+    label->box.pos = Vec(switchX + switchSpacingX -8, switchLabelY);
     label->text = "B";
     label->color = COLOR_BLACK;;
     addChild(label); 
    
     // 3 pos track_exp
-    addParam(ParamWidget::create<CKSSThree>( Vec(switchX,255), module, module->animator.TRACK_EXP_PARAM, 0.0f, 2.0f, 0.0f));
+    addParam(ParamWidget::create<CKSSThree>(
+        Vec(switchX + 2*switchSpacingX ,switchY), module, module->animator.TRACK_EXP_PARAM, 0.0f, 2.0f, 0.0f));
     label = new Label();
-    label->box.pos = Vec(switchX -12, 284);
+    label->box.pos = Vec(switchX + 2*switchSpacingX -12, switchLabelY);
     label->text = "Scl";
     label->color = COLOR_BLACK;
     addChild(label);  
 
-    // 3 pos LFO matrix
-    addParam(ParamWidget::create<CKSSThree>( Vec(switchX,matrixY), module, module->animator.LFO_MIX_PARAM, 0.0f, 2.0f, 0.0f));
-    label = new Label();
-    label->box.pos = Vec(switchX -15, matrixY+28);
-    label->text = "Mtx";
-    label->color = COLOR_BLACK;
-    addChild(label);  
+   
+
 
     /*************************************************
      *  screws
