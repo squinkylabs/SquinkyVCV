dbfd6852fe6af479e32a63101c92a3cdd2c79339
squinkylabs <squinkylabs@users.noreply.github.com>
2018-04-22 17:21:50 -0700

    -

--- dsp/fft/FFTData.h
+++ dsp/fft/FFTData.h
@@ -1,15 +1,28 @@
 #pragma once
 
+#include <complex>
+#include <vector>
 
+
+
+using cpx = std::complex<float>;
 
 class FFTDataCpx
 {
 public:
     FFTDataCpx(int numBins);
+    cpx get(int bin) const;
+    void set(int bit, cpx value);
+private:
+    std::vector<cpx> buffer;
 };
 
 class FFTDataReal
 {
 public:
     FFTDataReal(int numBins);
+    float get(int numBin) const;
+    void set(int numBin, float value);
+private:
+    std::vector<float> buffer;
 };
--- test/testFFT.cpp
+++ test/testFFT.cpp
@@ -5,7 +5,16 @@
 
 static void test0()
 {
-    FFTDataCpx d0(16);
+    FFTDataReal d0(16);
+
+    d0.set(0, 4);
+    assertEQ(d0.get(0), 4);
+
+    FFTDataCpx dc(16);
+
+    cpx x(3, 4);
+    dc.set(5, x);
+    assertEQ(dc.get(5), x);
 }
 
 void testFFT()
