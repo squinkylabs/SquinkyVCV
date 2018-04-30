e26d9951f8fe83866b904cfd1dfd09351b5abcec
squinkylabs <squinkylabs@users.noreply.github.com>
2018-04-29 18:07:57 -0700

    -

--- composites/ColoredNoise.h
+++ composites/ColoredNoise.h
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <memory>
+#include "assert.h"
+
+#include "ThreadClient.h"
+#include "ThreadServer.h"
+#include "ThreadSharedState.h"
+
 template <class TBase>
 class ColoredNoise : public TBase
 {
 public:
     ColoredNoise(struct Module * module) : TBase(module)
     {
+        commonConstruct();
     }
     ColoredNoise() : TBase()
     {
+        commonConstruct();
     }
     void setSampleRate(float rate)
     {
@@ -46,7 +55,34 @@ public:
     void step();
 
     typedef float T;        // use floats for all signals
+private:
+    std::unique_ptr<ThreadClient> thread;
+    void commonConstruct();
 };
+
+
+class NoiseServer : public ThreadServer
+{
+public:
+    NoiseServer(std::shared_ptr<ThreadSharedState> state) : ThreadServer(state)
+    {
+
+    }
+    virtual void handleMessage(ThreadMessage*)
+    {
+        assert(false);
+    }
+};
+
+template <class TBase>
+void ColoredNoise<TBase>::commonConstruct()
+{
+    std::shared_ptr<ThreadSharedState> threadState = std::make_shared<ThreadSharedState>();
+    std::unique_ptr<ThreadServer> server(new NoiseServer(threadState));
+
+    std::unique_ptr<ThreadClient> client(new ThreadClient(threadState, std::move(server)));
+    this->thread = std::move(client);
+}
 
 template <class TBase>
 void ColoredNoise<TBase>::step()
