4c3eaadc8b74818d4de8b2e141b852e14b8bb72a
squinkylabs <squinkylabs@users.noreply.github.com>
2018-05-30 22:34:48 -0700

    -

--- composites/Tremelo.h
+++ composites/Tremelo.h
@@ -1,6 +1,36 @@
 #pragma once
 
 #include "ClockMult.h"
+#include "ObjectCache.h"
+
+
+/*
+
+old plug proc loop.
+
+// Step 1: generate a saw
+// range is 0..1
+SawOsc<vec_t>::gen_v(*sawState, *sawParams, tempBuffer, sampleFrames);
+
+// step 2: apply skew and phase shift
+// range still 0..1
+AsymRampShaper<vec_t>::proc_v(*shaperParams, tempBuffer, tempBuffer, sampleFrames);
+
+// step 3: shift down to be centered at zero,
+// max excursion +-5 at shape "most square"
+// min is +/- .25  TODO: put the .25 into the control range itself
+
+// range = +/- (5 * shape)
+//
+f_t shapeMul = std::max(.25, 10 * controlValues.lfoShape);
+VecBasic<vec_t>::add_mul_c_imp(tempBuffer, sampleFrames, shapeMul, -.5f);
+
+// now tanh,
+// output contered around zero,
+// max is tanh(.25) to tanh(5), depending on shape value
+// rang = +/- tanh(5 * shape)
+LookupUniform<vec_t>::lookup_clip_v(*tanhParams, tempBuffer, tempBuffer, sampleFrames);
+*/
 
 /**
  */
@@ -68,7 +98,8 @@ public:
 private:
 
     ClockMult clock;
-
+    std::shared_ptr<LookupTableParams<float>> sinLookup;
+  //  getSinLookup();
     float reciprocalSampleRate = 0;;
 };
 
@@ -77,8 +108,13 @@ private:
 template <class TBase>
 inline void Tremelo<TBase>::init()
 {
+    sinLookup = ObjectCache<float>::getSinLookup();
     clock.setDivisor(0);
 }
+
+static float mx = -10;
+static float mn = 10;
+static int ct = 0;
 
 template <class TBase>
 inline void Tremelo<TBase>::step()
@@ -92,5 +128,17 @@ inline void Tremelo<TBase>::step()
     clock.setFreeRunFreq(r * reciprocalSampleRate);
     clock.sampleClock();
 
-    TBase::outputs[AUDIO_OUTPUT].value = clock.getSaw();   // just for now
+    float mod = clock.getSaw();
+    mod = LookupTable<float>::lookup(*sinLookup.get(), mod);
+
+ 
+    mn = std::min(mod, mn);
+    mx = std::max(mod, mx);
+
+    if (ct++ > 1000) {
+        printf("mod = %f, %f\n", mn, mx);
+        ct = 0;
+    }
+
+    TBase::outputs[AUDIO_OUTPUT].value = mod;   // just for now
 }
