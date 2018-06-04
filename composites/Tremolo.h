e444111448c21532d24956a0c8a97665fa6556c0
squinkylabs <squinkylabs@users.noreply.github.com>
2018-06-03 17:25:01 -0700

    working (a bit)

--- composites/Tremolo.h
+++ composites/Tremolo.h
@@ -4,6 +4,64 @@
 #include "ObjectCache.h"
 #include "AsymRampShaper.h"
 
+
+class Stats
+{
+public:
+    Stats(std::string s1, std::string s2, std::string s3)
+    {
+        labels.push_back(s1);
+        labels.push_back(s2);
+        labels.push_back(s3);
+        data.resize(3);
+        sum.resize(3);
+        for (int i=0; i<3; ++i) {
+            data[i].first = 100;
+            data[i].second = -100;
+            sum[i] = 0;
+        }
+    }
+    void log1(float x) { log(0, x); }
+    void log2(float x) { log(1, x); }
+    void log3(float x) { log(2, x); }
+private:
+    std::vector<std::pair<float, float>> data;
+    std::vector<std::string> labels;
+    std::vector<double> sum;
+    int dump_ct=0;
+    int total_ct=0;
+
+    void log(int index, float x)
+    {
+        #if 0
+        data[index].first = std::min(data[index].first, x);
+        data[index].second = std::max(data[index].second, x);
+        if (++dump_ct > 1000) {
+            dump();
+            dump_ct = 0;
+        }
+        sum[index] += x;
+        if (index == 0) {
+            ++total_ct;
+        }
+        #endif
+    }
+
+    void dump()
+    {
+        printf("\n");
+        for (int i=0; i<3; ++i) {
+            printf("%s: %f, %f avg=%f\n",
+                labels[i].c_str(),
+                data[i].first,
+                data[i].second,
+                sum[i] / total_ct);
+        }
+        fflush(stdout);
+    }
+
+};
+
 /*
 
 
@@ -85,7 +143,7 @@ template <class TBase>
 class Tremolo : public TBase
 {
 public:
-    Tremolo(struct Module * module) : TBase(module)
+    Tremolo(struct Module * module) : TBase(module), stats("saw", "mid", "final" )
     {
     }
     Tremolo() : TBase()
@@ -149,6 +207,8 @@ private:
     AudioMath::ScaleFun<float> scale_shape;
     AudioMath::ScaleFun<float> scale_depth;
     AudioMath::ScaleFun<float> scale_phase;
+
+    Stats stats;
 };
 
 
@@ -183,20 +243,17 @@ inline void Tremolo<TBase>::step()
     const float phase = scale_phase(0,
         TBase::params[LFO_PHASE_PARAM].value,
         1);
-        #if 0
+
     const float modDepth = scale_depth(0,
         TBase::params[MOD_DEPTH_PARAM].value,
         1);
-        #endif
+
    
 
     clock.setFreeRunFreq(rate * reciprocalSampleRate);
 
-
     // For now, call setup every sample. will eat a lot of cpu
     AsymRampShaper::setup(rampShaper, skew, phase);
-
-
 
     // ------------ now generate the lfo waveform
     clock.sampleClock();
@@ -206,14 +263,27 @@ inline void Tremolo<TBase>::step()
     // now we have a skewed saw -.5 to .5
     TBase::outputs[SAW_OUTPUT].value = mod;
 
+    stats.log1(mod);
+
     // TODO: don't scale twice - just get it right the first tme
     const float shapeMul = std::max(.25f, 10 * shape);
     mod *= shapeMul;
+    stats.log2(mod);
 
     mod = LookupTable<float>::lookup(*tanhLookup.get(), mod);
+    TBase::outputs[LFO_OUTPUT].value = mod;   
 
+    const float gain = modDepth / tanh(shapeMul/2);
+  //  const float finalMul = gain + 1;    // TODO: offset control?
+    const float finalMod = gain * mod + 1;      // TODO: this offset by 1 is pretty good, but we 
+                                                // could add an offset control
+    
+    stats.log3(finalMod);
+    TBase::outputs[AUDIO_OUTPUT].value = TBase::inputs[AUDIO_INPUT].value * finalMod;
 
-    TBase::outputs[LFO_OUTPUT].value = mod;   // just for now
+// for test
+   // TBase::outputs[AUDIO_OUTPUT].value = finalMod;
+
 }
 
 /*
@@ -243,4 +313,19 @@ VecBasic<vec_t>::add_mul_c_imp(tempBuffer, sampleFrames, shapeMul, -.5f);
 // max is tanh(.25) to tanh(5), depending on shape value
 // rang = +/- tanh(5 * shape)
 LookupUniform<vec_t>::lookup_clip_v(*tanhParams, tempBuffer, tempBuffer, sampleFrames);
+
+
+
+
+	// so: makeup gain of 1/tanh(shapeMul) will get us to +1/-1 
+	// then multiply by depth to get contered around zero with correct depth
+	// the add one to get back to trem range!
+	f_t gain = controlValues.modDepth / tanh(shapeMul/2);
+	VecBasic<vec_t>::mul_add_c_imp(tempBuffer, sampleFrames, gain, 1);
+// scale then add constant
+	// input = a * input + b
+	static void mul_add_c_imp(f_t * inout, int size, f_t a, f_t b) {
+		assert_size(size);
+
+	// now range = +/- tanh(5*shape) * depth / tanh(10 * shape)
 */
