a71870dac33c15abfc87d4fc97cf414a38e288b2
squinkylabs <squinkylabs@users.noreply.github.com>
2018-09-24 21:53:40 -0700

    -

--- src/WaveformSelector.h
+++ src/WaveformSelector.h
@@ -9,12 +9,42 @@ struct WaveformSelector  : OpaqueWidget
     ~WaveformSelector() override;
 
    // SVGWidget theSVG;
-   std::vector< SVGWidget> svgs;
+   std::vector< std::vector< SVGWidget>> svgs;
+   void addSvg(int row, const char* res);
+   void drawSVG(NVGcontext *vg, SVGWidget&);
 };
+
+inline void WaveformSelector::addSvg(int row, const char* res)
+{
+    if ((int)svgs.size() < row+1) {
+        svgs.resize(row+1);
+    }
+
+   // std::shared_ptr<SVG> w = SVG::load(assetPlugin(plugin, res));
+    SVGWidget w;
+    w.setSVG( SVG::load(assetPlugin(plugin, res)));
+    svgs[row].push_back(w);
+    
+    float y = 0;
+    if (row > 0) {
+        assert(!svgs[row-1].empty());
+        const SVGWidget& svg = svgs[row-1][0];
+        y = svg.box.pos.y + svg.box.size.y;
+    }
+    if (svgs[row].size() == 1) {
+        w.box.pos.x = 0;
+        w.box.pos.y = y;
+        // still need to find y offset
+    }
+}
 
 inline WaveformSelector::WaveformSelector()
 {
     printf("in ctor of selector\n"); fflush(stdout);
+    addSvg(0, "res/saw_wave.svg");
+    addSvg(0, "res/saw_wave.svg");
+
+    #if 0
     svgs.resize(2);
     svgs[0].setSVG( SVG::load(assetPlugin(plugin, "res/saw_wave.svg")));
     svgs[1].setSVG( SVG::load(assetPlugin(plugin, "res/saw_wave.svg")));
@@ -23,8 +53,9 @@ inline WaveformSelector::WaveformSelector()
 
     this->box.size.x = svgs[0].box.size.x * 2;
     this->box.size.y = svgs[0].box.size.y;
+    #endif
 
-#if 1
+#if 0
     printf("svg0 xy=%.2f,%.2f w=%.2f\n", 
         svgs[0].box.pos.x,
         svgs[0].box.pos.y,
@@ -51,15 +82,24 @@ inline WaveformSelector::~WaveformSelector()
     printf("in dtor of waveform selector");
 }
 
- void inline WaveformSelector:: draw(NVGcontext *vg)
- {
-     #if 0
-     auto p = theSVG.box.pos;
-     auto s = theSVG.box.size;
-     printf("pos x = %f, size x = %f\n", p.x, s.x);
-     #endif
-    // theSVG.draw(vg);
+inline void WaveformSelector::drawSVG(NVGcontext *vg, SVGWidget& svg)
+{
+    float transform[6];
+    nvgTransformIdentity(transform);
+    nvgTransformTranslate(transform, svg.box.size.x, 0);
+    nvgTransform(vg, transform[0], transform[1], transform[2], transform[3], transform[4], transform[5]);
 
+    svg.draw(vg);
+
+}
+void inline WaveformSelector::draw(NVGcontext *vg)
+{
+    for (auto r : svgs) {
+        for (auto s : r) {
+            drawSVG(vg, s);
+        }
+    }
+    #if 0
     svgs[1].draw(vg);
     float transform[6];
     nvgTransformIdentity(transform);
@@ -67,9 +107,5 @@ inline WaveformSelector::~WaveformSelector()
     nvgTransform(vg, transform[0], transform[1], transform[2], transform[3], transform[4], transform[5]);
 
     svgs[1].draw(vg);
-    #if 0
-    for (auto svg : this->svgs) {
-        svg.draw(vg);
-    }
     #endif
- }
+}
