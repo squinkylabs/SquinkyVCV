cf079cdbe0e8a2c0cd1f1016e63e3768412aba38
squinkylabs <squinkylabs@users.noreply.github.com>
2018-09-25 18:52:58 -0700

    -

--- src/WaveformSelector.h
+++ src/WaveformSelector.h
@@ -2,25 +2,44 @@
 
 #include "widgets.hpp"
 
+class ButtonCell
+{
+public:
+    friend class WaveformSelector;
+    ButtonCell(const ButtonCell&) = delete;
+    ButtonCell& operator = (const ButtonCell&) = delete;
+
+    ButtonCell(float x) : value(x) {}
+    void loadSVG(const char* res, const char* resOn);
+
+    const float value;
+    rack::Rect box;
+private:
+    SVGWidget svg;
+    SVGWidget svgOn;
+
+};
+
+inline void ButtonCell::loadSVG(const char* res, const char* resOn)
+{
+    svg.setSVG(SVG::load (assetPlugin(plugin, res)));
+    svgOn.setSVG(SVG::load (assetPlugin(plugin, resOn)));
+    this->box.size = svg.box.size;
+}
+
+using CellPtr = std::shared_ptr<ButtonCell>;
+
 struct WaveformSelector  : OpaqueWidget
 {
-    struct Cell
-    {
-        Cell(float x) : value(x) {}
-        std::shared_ptr<SVGWidget> svg;
-        std::shared_ptr<SVGWidget> svgOn;
-        const float value;
-    };
-
     WaveformSelector();
     void draw(NVGcontext *vg) override;
     ~WaveformSelector() override;
 
-    std::vector< std::vector< Cell>> svgs;
+    std::vector< std::vector< CellPtr>> svgs;
     void addSvg(int row, const char* res, const char* resOn);
     void drawSVG(NVGcontext *vg, SVGWidget&);
     void onMouseDown( EventMouseDown &e ) override;
-    Cell* hitTest(float x, float y);
+    CellPtr hitTest(float x, float y);
     //
     float nextValue = 1;
 
@@ -32,21 +51,23 @@ struct WaveformSelector  : OpaqueWidget
     float curValue=0;
 };
 
- WaveformSelector::Cell* WaveformSelector::hitTest(float x, float y)
+ CellPtr WaveformSelector::hitTest(float x, float y)
  {
+     assert(false);
+
     const Vec pos(x, y);
     for (auto& r : svgs) {
         for (auto& s : r) {
-#if 0
+            #if 0
             printf("  v=%f svg = x-%f, y-%f, w-%f, h-%f\n",
                 s.value,
                 s.svg->box.pos.x,
                 s.svg->box.pos.y,
                 s.svg->box.size.x,
                 s.svg->box.size.y);
-                #endif
-            if (s.svg->box.contains(pos)) {
-                return &s;
+            #endif
+            if (s->box.contains(pos)) {
+                return s;
             }
         }
     }
@@ -59,61 +80,61 @@ inline void WaveformSelector::addSvg(int row, const char* res, const char* resOn
     if ((int)svgs.size() < row+1) {
         svgs.resize(row+1);
     }
+   
 
-    // SVGWidget w;
-    //  std::create_shared<SVGWidget>;
-    std::shared_ptr<SVGWidget> p(new SVGWidget());
-
-    Cell cell(nextValue++);
-    p->setSVG( SVG::load(assetPlugin(plugin, res)));
-    cell.svg = p;
-
+    // make a new cell, put the SVGs in it
+    CellPtr cell = std::make_shared<ButtonCell>(nextValue++);
+    cell->loadSVG(res, resOn);
     svgs[row].push_back(cell);
-    
+
+ 
+    // now set the box for cell
     float y = 0;
     if (row > 0) {
+        // if we are going in the second row, y = height of first
         assert(!svgs[row-1].empty());
-        auto svg = svgs[row-1][0].svg;
-        y = svg->box.pos.y + svg->box.size.y;
+        CellPtr otherCell = svgs[row-1][0];
+        y = otherCell->box.pos.y + otherCell->box.size.y;
     }
-    cell.svg->box.pos.y = y;
-    if (svgs[row].size() == 1) {
-        cell.svg->box.pos.x = 0;
-        printf("just set x to 0 val=%f\n", cell.value);
-    } else {
-        cell.svg->box.pos.x = 
-            svgs[row].back().svg->box.pos.x +
-            svgs[row].back().svg->box.size.x;
-        printf("just set x to %f value=%f\n", cell.svg->box.pos.x, cell.value);
-    }
+    cell->box.pos.y = y;
 
+    if (svgs[row].size() == 1) {
+        // If we are the first cell in this row, we are at 0
+        cell->box.pos.x = 0;
+        printf("just set x to 0 val=%f\n", cell->value);
+    } else {
+        cell->box.pos.x = 
+            svgs[row].back()->box.pos.x +
+            svgs[row].back()->box.size.x;
+        printf("just set x to %f value=%f\n", cell->box.pos.x, cell->value);
+    }
+#if 0
     // Now load SVG for the on state
     std::shared_ptr<SVGWidget> p2(new SVGWidget());
     p2->setSVG( SVG::load(assetPlugin(plugin, resOn)));
     cell.svgOn = p2;
     cell.svgOn->box.pos = cell.svg->box.pos; 
+#endif
 
-    printf("svgon box size=%f, %f po %f, %f\n",
-        cell.svgOn->box.size.x,
-        cell.svgOn->box.size.y,
-        cell.svgOn->box.pos.x,
-        cell.svgOn->box.pos.y);
-
-    printf("load cell, svg=%p on=%p\n", cell.svg.get(), cell.svgOn.get());
+    printf("cell box size=%f, %f po %f, %f\n",
+        cell->box.size.x,
+        cell->box.size.y,
+        cell->box.pos.x,
+        cell->box.pos.y); 
 }
 
 inline WaveformSelector::WaveformSelector()
 {
+
     addSvg(0, "res/saw_wave.svg", "res/saw_wave_on.svg" );
     addSvg(0, "res/saw_wave.svg", "res/saw_wave_on.svg");
 
     for (int i=0; i<2; ++i) {
         printf("col %d. %p,%p\n",
             i,
-            svgs[0][i].svg.get(),
-            svgs[0][2].svgOn.get());
+            &svgs[0][i]->svg,
+            &svgs[0][2]->svgOn);
         fflush(stdout);
-
     }
 }
 
@@ -135,16 +156,9 @@ void inline WaveformSelector::draw(NVGcontext *vg)
 {
     for (auto& r : svgs) {
         for (auto& s : r) {
-            const bool on = (curValue == s.value);
-            if (!s.svg) {
-                printf("No svg\n"); fflush(stdout);
-                return;
-            }
-             if (!s.svgOn) {
-               // printf("No svg On\n"); fflush(stdout);
-                return;
-            }
-            drawSVG(vg, on ? *s.svgOn : *s.svg);
+            const bool on = (curValue == s->value);
+            drawSVG(vg, on ? s->svgOn : s->svg);
+
         }
     }
 }
@@ -153,7 +167,8 @@ inline void WaveformSelector::onMouseDown( EventMouseDown &e )
 {
     e.consumed = false;
     printf("mouse down %f, %f\n", e.pos.x, e.pos.y); fflush(stdout);
-    Cell* hit = hitTest(e.pos.x, e.pos.y);
+ 
+    CellPtr hit = hitTest(e.pos.x, e.pos.y);
     if (hit) {
         e.consumed = true;
         printf("hit test found cell\n"); fflush(stdout);
@@ -166,4 +181,5 @@ inline void WaveformSelector::onMouseDown( EventMouseDown &e )
     } else {
         printf("hit test failed\n"); fflush(stdout);
     }
+
 }
