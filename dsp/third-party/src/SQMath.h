2185b97dcbcf0a5d7f472a61ee58f425ae26fa9b
squinkylabs <squinkylabs@users.noreply.github.com>
2019-01-05 12:29:41 -0800

    back-port to 0.6

--- dsp/third-party/src/SQMath.h
+++ dsp/third-party/src/SQMath.h
@@ -1,24 +1,45 @@
 #pragma once
 
-#include "math.hpp"
+#ifdef _V1
+    #include "math.hpp"
+#else
+    #include "util/math.hpp"
+#endif
 
+
+/**
+ * A wrapper around rack's math functions.
+ * Mitigates some V1 vs V06 issues.
+ */
 namespace sq 
 {
 
 inline float clamp(float a, float b, float c)
 {
-    return rack::math::clamp(a, b, c);
+    #ifdef _V1 
+        return rack::math::clamp(a, b, c);
+    #else
+        return rack::clamp(a, b, c);
+    #endif
 }
 
 inline float interpolateLinear(float* a, float b)
 {
-    return rack::math::interpolateLinear(a, b);
+    #ifdef _V1
+        return rack::math::interpolateLinear(a, b);
+    #else
+        return rack::interpolateLinear(a, b);
+    #endif
 }
 
 
 inline float rescale(float a, float b, float c, float d, float e)
 {
-    return rack::math::rescale(a, b, c, d, e);
+    #ifdef _V1
+        return rack::math::rescale(a, b, c, d, e);
+    #else
+        return rack::rescale(a, b, c, d, e);
+    #endif
 }
 
 }
