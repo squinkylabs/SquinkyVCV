def2fb8bda46f4f044494f788992a0ce3809560e
squinkylabs <squinkylabs@users.noreply.github.com>
2019-02-10 14:39:07 -0800

    -

--- test/testIComposite.cpp
+++ test/testIComposite.cpp
@@ -1,15 +1,23 @@
 
 #include "LFN.h"
 #include "asserts.h"
+#include "CHB.h"
 #include "FunVCOComposite.h"
+#include "Gray.h"
+#include "Shaper.h"
 #include "TestComposite.h"
+#include "VocalFilter.h"
+
+#include <set>
 
 
 template <class Comp>
 inline static void test()
 {
-    std::shared_ptr<IComposite> comp(new Comp());
+    std::shared_ptr<IComposite> comp = Comp::getDescription();
 
+    std::set<std::string> names;
+    assertEQ(comp->getNumParams(), Comp::NUM_PARAMS);
     assertGT(comp->getNumParams(), 0);
     for (int i = 0; i < comp->getNumParams(); ++i)
     {
@@ -17,6 +25,14 @@ inline static void test()
         assertLT(config.min, config.max);
         assertLE(config.def, config.max);
         assertGE(config.def, config.min);
+
+        assert(config.name);
+        std::string name = config.name;
+        assert(!name.empty());
+
+        // make sure they are unique
+        assert(names.find(name) == names.end());
+        names.insert(name);
     }
 }
 
@@ -24,4 +40,8 @@ void testIComposite()
 {
     test<FunVCOComposite<TestComposite>>();
     test<LFN<TestComposite>>();
+    test<VocalFilter<TestComposite>>();
+    test<Shaper<TestComposite>>();
+    test<CHB<TestComposite>>();
+    test<Gray<TestComposite>>();
 }
