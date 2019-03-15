28faa001d054c73dab723f51437974f3f5b7f0b3
squinkylabs <squinkylabs@users.noreply.github.com>
2019-03-14 21:50:02 -0700

    -

--- dsp/utils/fVec.h
+++ dsp/utils/fVec.h
@@ -11,6 +11,8 @@ public:
     float* get();
 
     void zero();
+    void add_i(const fVec<N>& other);
+    void mul_i(const fVec<N>& other);
 };
 
 template <int N>
@@ -21,9 +23,24 @@ inline void fVec<N>::zero()
     }
 }
 
-
 template <int N>
 inline float* fVec<N>::get()
 {
     return reinterpret_cast<float*> (&data[0]);
+}
+
+template <int N>
+inline void fVec<N>::add_i(const fVec<N>& other)
+{
+    for (int i = 0; i < N; ++i) {
+        data[i] = _mm_add_ps(data[i], other.data[i]);
+    }
+}
+
+template <int N>
+inline void fVec<N>::mul_i(const fVec<N>& other)
+{
+    for (int i = 0; i < N; ++i) {
+        data[i] = _mm_mul_ps(data[i], other.data[i]);
+    }
 }
