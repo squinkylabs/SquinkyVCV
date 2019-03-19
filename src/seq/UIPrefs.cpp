34a5c49ac7dd9de5747afae136e1d5f899fb1758
squinkylabs <squinkylabs@users.noreply.github.com>
2019-03-18 22:01:48 -0700

    -

--- src/seq/UIPrefs.cpp
+++ src/seq/UIPrefs.cpp
@@ -17,4 +17,9 @@ const NVGcolor UIPrefs::GRID_BAR_COLOR =
 
 const NVGcolor UIPrefs::TIME_LABEL_COLOR = nvgRGB(0xb0, 0xb0, 0xb0);
 const NVGcolor UIPrefs::GRID_CLINE_COLOR = nvgRGB(0x60, 0x60, 0x60);
-const NVGcolor  UIPrefs::STATUS_LABEL_COLOR = nvgRGB(0xd0, 0xd0, 0xd0);//TIME_LABEL_COLOR;
+const NVGcolor  UIPrefs::STATUS_LABEL_COLOR = nvgRGB(0xd0, 0xd0, 0xd0);
+
+const float UIPrefs::hMarginsNoteEdit = 2.f;
+const float UIPrefs::topMarginNoteEdit = 0.f;
+const float UIPrefs::timeLabelFontSize = 11.f;
+
