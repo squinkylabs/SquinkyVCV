c580551811cd1cb490c28a71284e5d5ee8a899d7
squinkylabs <squinkylabs@users.noreply.github.com>
2019-04-15 17:58:47 -0700

    -

--- sqsrc/util/CommChannels.h
+++ sqsrc/util/CommChannels.h
@@ -9,6 +9,7 @@
 class CommChannelSend
 {
 public:
+    const static int zeroPad = 3;       // number of zeros to send out after a command
     /**
      * Queues up a message to be sent.
      * Will be sent over several periods.
@@ -24,6 +25,7 @@ private:
     SqRingBuffer<uint32_t, 4> messageBuffer;
     bool sendingData = false;
     bool sendingZero = false;
+    int zeroCount = 0;
 };
 
 
@@ -33,7 +35,11 @@ private:
  */
 class CommChannelReceive
 {
-
+public:
+    /**
+     * returns zero if no data received, otherwise returns data
+     */
+    uint32_t rx(const uint32_t * inputBuffer);
 };
 
 
@@ -48,10 +54,14 @@ void CommChannelSend::go(uint32_t* output)
 {
     uint32_t x=0;
     if (sendingZero) {
-      
-        sendingZero = false;
+        if (++zeroCount >= zeroPad) {
+            sendingZero = false;
+        }
     } else if (sendingData) {
         sendingData = false;
+        sendingZero = true;
+        zeroCount = 1;
+
         x = 0;
     } else {
         if (!messageBuffer.empty()) {
@@ -75,3 +85,9 @@ void CommChannelSend::go(uint32_t* output)
     *output = x;
 }
 #endif
+
+uint32_t CommChannelReceive::rx(const uint32_t * inputBuffer)
+{
+    uint32_t ret = inputBuffer[0];
+    return ret;
+};
--- test/testCommChannels.cpp
+++ test/testCommChannels.cpp
@@ -4,6 +4,20 @@
 
 uint32_t buffer[1];
 
+#if 0
+static void debug()
+{
+    CommChannelSend ch;
+    ch.send(100);
+    ch.send(200);
+    ch.send(300);
+    for (int i = 0; i < 15; ++i) {
+        ch.go(buffer);
+        printf("output[%d] = %d\n", i, buffer[0]);
+    }
+}
+#endif
+
 static void testSend1()
 {
     buffer[0] = 13;
@@ -27,6 +41,7 @@ static void testSend2()
 
     uint32_t test1 = 0xaa551122;
     uint32_t test2 = 0xaa551123;
+    uint32_t test3 = 0xaa551124;
 
     CommChannelSend ch;
     ch.go(buffer);
@@ -34,21 +49,61 @@ static void testSend2()
 
     ch.send(test1);
     ch.send(test2);
+    ch.send(test3);
     ch.go(buffer);
     assertEQ(buffer[0], test1);
 
-    ch.go(buffer);
-    assertEQ(buffer[0], 0);
+    for (int i = 0; i < CommChannelSend::zeroPad; ++i) {
+        ch.go(buffer);
+        assertEQ(buffer[0], 0);
+    }
 
     ch.go(buffer);
     assertEQ(buffer[0], test2);
 
+    for (int i = 0; i < CommChannelSend::zeroPad; ++i) {
+        ch.go(buffer);
+        assertEQ(buffer[0], 0);
+    }
+
     ch.go(buffer);
-    assertEQ(buffer[0], 0);
+    assertEQ(buffer[0], test3);
+
+    for (int i = 0; i < CommChannelSend::zeroPad +10; ++i) {
+        ch.go(buffer);
+        assertEQ(buffer[0], 0);
+    }
+
 }
+
+static void testRx0()
+{
+    CommChannelReceive ch;
+    buffer[0] = 0;
+    for (int i = 0; i < 20; ++i) {
+        assertEQ(ch.rx(buffer), 0);
+    }
+}
+
+
+static void testRx1()
+{
+    CommChannelReceive ch;
+    buffer[0] = 0;
+    for (int i = 0; i < 4; ++i) {
+        assertEQ(ch.rx(buffer), 0);
+    }
+
+    buffer[0] = 55;
+    assertEQ(ch.rx(buffer), 55);
+}
+
 
 void testCommChannels()
 {
+   // debug();
     testSend1();
     testSend2();
+    testRx0();
+    testRx1();
 }
