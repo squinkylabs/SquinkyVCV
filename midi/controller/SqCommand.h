857a8b007d215ebd707d126d2cf0060831d5924c
squinkylabs <squinkylabs@users.noreply.github.com>
2019-05-28 22:51:28 -0700

    cleanup

--- midi/controller/SqCommand.h
+++ midi/controller/SqCommand.h
@@ -8,6 +8,7 @@ using MidiSequencerPtr = std::shared_ptr<MidiSequencer>;
 class SqCommand
 {
 public:
+    virtual ~SqCommand() {}
     virtual void execute(MidiSequencerPtr seq) = 0;
     virtual void undo(MidiSequencerPtr seq) = 0;
     std::string name = "Seq++";
