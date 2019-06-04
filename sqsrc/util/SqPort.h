ba52e78a654f1b4569dd2e6733a5dd78c5982d11
squinkylabs <squinkylabs@users.noreply.github.com>
2019-06-03 21:55:45 -0700

    -

--- sqsrc/util/SqPort.h
+++ sqsrc/util/SqPort.h
@@ -1,6 +1,8 @@
 #pragma once
 
 // Helper class to make ports compatible between 0.6 and 1.0
+
+#ifdef __V1x
 class SqPort
 {
 public:
@@ -11,3 +13,23 @@ inline bool SqPort::isConnected(Port& input)
 {
     return input.isConnected();
 }
+#else
+
+class SqPort
+{
+public:
+    static bool isConnected(Input& input);
+    static bool isConnected(Output& input);
+};
+
+inline bool SqPort::isConnected(Input& input)
+{
+    return input.active;
+}
+
+inline bool SqPort::isConnected(Output& output)
+{
+    return output.active;
+}
+
+#endif
