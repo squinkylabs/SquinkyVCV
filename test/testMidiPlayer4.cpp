a542ef88d721aa3cb27f5e1632ddb0179b1fd292
squinkylabs <squinkylabs@users.noreply.github.com>
2020-02-09 15:38:23 -0800

    -

--- test/testMidiPlayer4.cpp
+++ test/testMidiPlayer4.cpp
@@ -88,64 +88,6 @@ static void testTwoSections(int trackNum)
     assertEQ(host->cvValue[0], PitchUtils::pitchToCV(4, PitchUtils::c));
 }
 
-static void testTwoSectionsStartOnSecond()
-{
-    printf("---- testTwoSectionsStartOnSecond\n");
-    const int trackNum = 0;
-    MidiSong4Ptr song = makeSong(trackNum);
-    std::shared_ptr<TestHost4> host = std::make_shared<TestHost4>();
-    MidiPlayer4 pl(host, song);
-
-    const float quantizationInterval = .01f;
-    pl.setNextSection(trackNum, 2);     // skip the first section 
-                                        // (request index == 1)
-    assertEQ(pl.getNextSection(trackNum), 2);
-
-    const float startOffset = 4;
-    pl.setRunningStatus(true);
-
-    // second section, first note (c at 0)
-    pl.updateToMetricTime(4.1 - startOffset, quantizationInterval, true);
-
-    assertEQ(pl.getSection(trackNum), 2);       // we sent 2 to request 1 (2)
-    assertEQ(host->gateChangeCount, 1);
-    assertEQ(host->gateState[0], true);
-    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
-    host->assertOneActiveTrack(trackNum);
-
-    printf("finish testTwoSectionsStartOnSecond\n");
-#if 0
-    assert(false);      // the rest needs porting. or something.
-
-    // second section, after first note (c at 0)
-    pl.updateToMetricTime(4.6, quantizationInterval, true);
-    host->assertOneActiveTrack(trackNum);
-    assertEQ(host->gateChangeCount, 4);
-    assertEQ(host->gateState[0], false);
-    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
-
-    // second section, second note (d at 1)
-    pl.updateToMetricTime(5, quantizationInterval, true);
-    host->assertOneActiveTrack(trackNum);
-    assertEQ(host->gateChangeCount, 5);
-    assertEQ(host->gateState[0], true);
-    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
-
-    // second section, after second note (d at 1)
-    pl.updateToMetricTime(5.6, quantizationInterval, true);
-    host->assertOneActiveTrack(trackNum);
-    assertEQ(host->gateChangeCount, 6);
-    assertEQ(host->gateState[0], false);
-    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
-
-    // now all the way to the last note in the last section
-    pl.updateToMetricTime(4 + 7, quantizationInterval, true);
-    host->assertOneActiveTrack(trackNum);
-    assertEQ(host->gateState[0], true);
-    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(4, PitchUtils::c));
-#endif
-}
-
 static void testTwoSectionsLoop()
 {
     const int trackNum = 0;
@@ -305,6 +247,157 @@ static void testRepeatReset()
     assertGT(host->gateChangeCount, ct0 + 4);
 }
 
+
+
+static void testTwoSectionsStartOnSecond()
+{
+    const int trackNum = 0;
+    MidiSong4Ptr song = makeSong(trackNum);
+    std::shared_ptr<TestHost4> host = std::make_shared<TestHost4>();
+    MidiPlayer4 pl(host, song);
+
+    const float quantizationInterval = .01f;
+    pl.setNextSection(trackNum, 2);     // skip the first section 
+                                        // (request index == 1)
+    assertEQ(pl.getNextSection(trackNum), 2);
+
+    const float startOffset = 4;
+    pl.setRunningStatus(true);
+
+    // second section, first note (c at 0)
+    pl.updateToMetricTime(4.1 - startOffset, quantizationInterval, true);
+
+    assertEQ(pl.getSection(trackNum), 2);       // we sent 2 to request 1 (2)
+    assertEQ(host->gateChangeCount, 1);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
+    host->assertOneActiveTrack(trackNum);
+
+    printf("finish testTwoSectionsStartOnSecond\n");
+#if 0
+    assert(false);      // the rest needs porting. or something.
+
+    // second section, after first note (c at 0)
+    pl.updateToMetricTime(4.6, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 4);
+    assertEQ(host->gateState[0], false);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
+
+    // second section, second note (d at 1)
+    pl.updateToMetricTime(5, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 5);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
+
+    // second section, after second note (d at 1)
+    pl.updateToMetricTime(5.6, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 6);
+    assertEQ(host->gateState[0], false);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
+
+    // now all the way to the last note in the last section
+    pl.updateToMetricTime(4 + 7, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(4, PitchUtils::c));
+#endif
+}
+
+
+static void testTwoSectionsSwitchToSecond()
+{
+    const int trackNum = 0;
+    MidiSong4Ptr song = makeSong(trackNum);
+    auto option = song->getOptions(trackNum, 0);
+    assert(option);
+    option->repeatCount = 10;                       // make the first section repeat a long time
+    std::shared_ptr<TestHost4> host = std::make_shared<TestHost4>();
+    MidiPlayer4 pl(host, song);
+
+
+ //   pl.setNextSection(trackNum, 2);     // skip the first section 
+                                        // (request index == 1)
+   // assertEQ(pl.getNextSection(trackNum), 2);
+
+  
+    pl.setRunningStatus(true);
+
+    // determine we are playing section 1
+    const float quantizationInterval = .01f;
+    pl.updateToMetricTime(.8, quantizationInterval, true);
+    assertEQ(host->gateChangeCount, 0);
+    assertEQ(host->gateState[0], false);
+
+    // after note, 1
+    pl.updateToMetricTime(1.1, quantizationInterval, true);
+    assertEQ(host->gateChangeCount, 1);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->cvValue[0], 7.5f);
+    assertEQ(host->gateState[0], true);
+
+    // after end of note
+    pl.updateToMetricTime(3.8, quantizationInterval, true);
+    assertEQ(host->gateChangeCount, 2);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->cvValue[0], 7.5f);
+    assertEQ(host->gateState[0], false);
+
+    // request next section
+    pl.setNextSection(trackNum, 2);     // skip the first section 
+                                      // (request index == 1)
+
+    // verify we are in second pattern
+ 
+
+
+    // second section, first note (c at 0)
+    pl.updateToMetricTime(4.1, quantizationInterval, true);
+
+    assertEQ(pl.getSection(trackNum), 2);       // we sent 2 to request 1 (2)
+    assertEQ(host->gateChangeCount, 3);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
+    host->assertOneActiveTrack(trackNum);
+
+    printf("finish testTwoSectionsSwitchToSecond\n");
+#if 0
+    assert(false);      // the rest needs porting. or something.
+
+    // second section, after first note (c at 0)
+    pl.updateToMetricTime(4.6, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 4);
+    assertEQ(host->gateState[0], false);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::c));
+
+    // second section, second note (d at 1)
+    pl.updateToMetricTime(5, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 5);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
+
+    // second section, after second note (d at 1)
+    pl.updateToMetricTime(5.6, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateChangeCount, 6);
+    assertEQ(host->gateState[0], false);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(3, PitchUtils::d));
+
+    // now all the way to the last note in the last section
+    pl.updateToMetricTime(4 + 7, quantizationInterval, true);
+    host->assertOneActiveTrack(trackNum);
+    assertEQ(host->gateState[0], true);
+    assertEQ(host->cvValue[0], PitchUtils::pitchToCV(4, PitchUtils::c));
+#endif
+}
+
+
+//**************** API tests *******
+
 static void testSection12()
 {
     const int trackNum = 0;
@@ -363,13 +456,17 @@ static void testSectionApi()
     testSectionStartOffset();
 }
 
+
+
 void testMidiPlayer4()
 {
     testSectionApi();
     testTwoSections(0);
     testTwoSections(3);
     testTwoSectionsStartOnSecond();
+    testTwoSectionsSwitchToSecond();
     testTwoSectionsLoop();
     testTwoSectionsRepeat1();
     testRepeatReset();
 }
+   
