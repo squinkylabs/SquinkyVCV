dcba88074fc513454df8d250777ffbdb5acf55cb
squinkylabs <squinkylabs@users.noreply.github.com>
2020-02-15 17:13:11 -0800

    -

--- test/testMidiTrackPlayer.cpp
+++ test/testMidiTrackPlayer.cpp
@@ -1,6 +1,22 @@
+
+#include "MidiSong4.h"
+#include "MidiTrackPlayer.h"
+#include "TestHost2.h"
 #include "asserts.h"
+
+static void testCanCall()
+{
+    std::shared_ptr<IMidiPlayerHost4> host = std::make_shared<TestHost2>();
+    std::shared_ptr<MidiSong4> song = std::make_shared<MidiSong4>();
+    MidiTrackPlayer pl(host, 0, song);
+
+    const float quantizationInterval = .01f;
+    bool b = pl.playOnce(1, quantizationInterval);
+    assert(!b);
+}
 
 void testMidiTrackPlayer()
 {
-    assert(false);
+    testCanCall();
+   
 }
