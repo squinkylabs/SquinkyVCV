82adbfd7f269a93b3d9dd259040b5066580556eb
bgf777 <bruce@mowrite.net>
2020-02-23 14:31:28 -0800

    -

--- src/seq/ClockFinder.cpp
+++ src/seq/ClockFinder.cpp
@@ -56,27 +56,32 @@ static std::vector<PortWidget*> findSeqInputs(ModuleWidget* seq) {
      return (found == 3 ) ? ret : std::vector<PortWidget*>();
 }
 
-static std::vector<PortWidget*> findClockedOutputs(ModuleWidget* clocked) {
-    int found = 0;
-     std::vector<PortWidget*> ret(3);
+// Since we already know which one to use for clock, we pass that int
+static std::vector<PortWidget*> findClockedOutputs(ModuleWidget* clocked, PortWidget* clock) {
+    int found = 1;
+    std::vector<PortWidget*> ret(3);
 
-     for (auto output : clocked->outputs) {
-         switch(output->portId) {
-            case 1:
+    assert(clock);
+    ret[0] = clock;
+    for (auto output : clocked->outputs) {
+        switch(output->portId) {
+    #if 0
+            case 1:         // clock 1
                 ret[0] = output;
                 ++found;
                 break;
-            case 5:
+    #endif
+            case 5:         // run
                 ret[1] = output;
                 ++found;
                 break;
-            case 4:
+            case 4:         // reset
                 ret[2] = output;
                 ++found;
                 break;
-         }
-     }
-     return (found == 3 ) ? ret : std::vector<PortWidget*>();
+        }
+    }
+    return (found == 3 ) ? ret : std::vector<PortWidget*>();
 }
 
 static bool anyConnected(const std::vector<PortWidget*>& ports)
@@ -90,17 +95,50 @@ static bool anyConnected(const std::vector<PortWidget*>& ports)
     return false;
 }
 
+// index = 0 - clock 1 ... 2 - clock3 (skip master)
+PortWidget* findClockOutput(ModuleWidget* clocked, int index)
+{
+    for (auto output : clocked->outputs) {
+        if ((output->portId) == (index + 1)) {
+            return output;
+        }
+    }
+    assert(false);
+    return nullptr;
+}
+
+/**
+ * return.first will be a non null port widget to use for clock/
+ * return.second will be true if the port is already patched
+ */
+static std::pair<PortWidget*, bool> findBestClockOutput(ModuleWidget* clocked)
+{
+    for (int i=0; i<3; ++i) {
+        auto port = findClockOutput(clocked, i);
+        auto cables = APP->scene->rack->getCablesOnPort(port);
+        if (cables.empty()) {
+            return std::make_pair(port, false);
+        }
+    }
+     return std::make_pair(findClockOutput(clocked, 0), true);
+}
+
 void ClockFinder::go(ModuleWidget* host)
 {
     INFO("Clock Finder");
-    ModuleWidget* modwid = findClocked();
-    if (!modwid) {
+    ModuleWidget* clockedModule = findClocked();
+    if (!clockedModule) {
         return;
     }
 
-    auto outputs = findClockedOutputs(modwid);
+    auto clockOutput = findBestClockOutput(clockedModule);
+    assert(clockOutput.first != nullptr);
+
+    auto outputs = findClockedOutputs(clockedModule, clockOutput.first);
     auto inputs = findSeqInputs(host);
     if (outputs.size() != 3 || inputs.size() != 3) {
+
+        WARN("bad I/O matchup. o=%d, i=%d", outputs.size(), inputs.size());
         return;
     }
 
