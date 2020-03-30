5021af23c7efe413d12b0f51ece86d61e8a06c6c
bgf777 <bruce@mowrite.net>
2020-03-29 22:17:22 -0700

    -

--- composites/LadderFilterBank.h
+++ composites/LadderFilterBank.h
@@ -24,5 +24,14 @@ template <typename T>
 void LadderFilterBank<T>::step(int numChannels, bool stereoMode,
          SqInput& audioInput,  SqOutput& audioOutput)
 {
+    assert(!stereoMode);
+    for (int channel=0; channel < numChannels; ++channel) {
+        LadderFilter<T>& filt = filters[channel];
+
+        const float input =audioInput.getVoltage(channel);
+            filt.run(input);
+            const float output = (float) filt.getOutput();
+            audioOutput.setVoltage(output, channel);
+    }
     
 }
