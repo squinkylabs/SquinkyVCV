3899f42bfa954300ad1a7f3930172a88226be7e7
bgf777 <bruce@mowrite.net>
2020-05-12 22:58:54 -0700

    -

--- composites/WVCO.h
+++ composites/WVCO.h
@@ -27,14 +27,31 @@ class WVCODsp
 {
 public:
     float_4 step(float_4 freq) {
+        static int times =0;
+        times++;
+       
+        
         phaseAcc += freq;
+
+        const int limit = 0;
+         if (times < limit) {
+            printf("freq = %.2f, %.2f, %.2f, %.2f\n", freq[0], freq[1], freq[2], freq[3]);
+            printf("ph = %.2f, %.2f, %.2f, %.2f\n", phaseAcc[0], phaseAcc[1], phaseAcc[2], phaseAcc[3]);
+        }
 
 		// Wrap phase
 		phaseAcc -= simd::floor(phaseAcc);
-        return phaseAcc;
+
+        const __m128 twoPi = _mm_set_ps1(2 * 3.141592653589793238);
+        float_4 s = rack::simd::sin(phaseAcc * twoPi);
+      // float_4 s = phaseAcc;
+         if (times < limit) {
+            printf("s = %.2f, %.2f, %.2f, %.2f\n", s[0], s[1], s[2], s[3]);
+         }
+        return s;
     }
 private:
-    float_4 phaseAcc;
+    float_4 phaseAcc = float_4::zero();
 };
 
 template <class TBase>
@@ -109,7 +126,7 @@ inline void WVCO<TBase>::step()
 {
     for (int bank=0; bank<4; ++bank) {
         const int channel = 4 * bank;
-        float_4 v = dsp[bank].step(.005);
+        float_4 v = dsp[bank].step(.005);       // was .005
         WVCO<TBase>::outputs[MAIN_OUTPUT].setVoltageSimd(v, channel);
     }
     // why can't I set to 16?
