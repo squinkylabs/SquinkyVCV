9178af88353105410ff99c9f0d5e09172d497990
bgf777 <bruce@mowrite.net>
2020-05-17 13:18:23 -0700

    -

--- test/testWVCO.cpp
+++ test/testWVCO.cpp
@@ -34,7 +34,7 @@ static void testTriFormula2()
     }
 }
 
-static void testADSR()
+static void testADSR0()
 {
     ADSR4 adsr;
     adsr.setA(0);
@@ -42,6 +42,21 @@ static void testADSR()
     adsr.setS(0);
     adsr.setR(0);
     adsr.step(0); 
+}
+
+static void testADSR1()
+{
+    // very fast envelope
+    ADSR4 adsr;
+    adsr.setA(0);
+    adsr.setD(0);
+    adsr.setS(1);
+    adsr.setR(0);
+
+    // gat it, should get something out
+    float_4 high(2);
+    float_4 out = adsr.step(high); 
+    simd_assertGT(out, float_4(1));
 }
 
 static void testPumpData()
@@ -72,5 +87,6 @@ void testWVCO()
     testTriFormula();
     testTriFormula2();
     testPumpData();
-    testADSR();
+    testADSR0();
+    testADSR1();
 }
