8d01f9b05615c6edde6113e81d8451ffd78b2f32
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2020-07-18 15:58:49 -0700

    -

--- test/testDC.cpp
+++ test/testDC.cpp
@@ -2,21 +2,33 @@
 
 
 using oscillator = std::function<float()>;
-void vcoProfiler(bool highToLow, oscillator osc)
+void vcoProfiler(bool highToLow, int minimumSamples, oscillator osc)
 {
     float last = osc();
     bool done = false;
     int ct = 0;
+    float sum = 0;
+    int samples = 0;
+    bool firstCross = true;
+
+
     while(!done) {
         float x = osc();
-        printf("in loop, x = %f last = %f\n", x, last);
+        //printf("in loop, x = %f last = %f\n", x, last);
         bool crossed = highToLow ? 
             (x < (last - .5)) :
             (x > (last + .5));
 
         if (crossed) {
             printf("crossed at %d\n", ct);
-            done = true;
+            if (firstCross) {
+                firstCross = false;
+                sum = 0;
+                samples = 0;
+            } else {
+                printf("finished. sum = %f norm= %f\n", sum, sum / samples);
+                done = samples > minimumSamples;
+            }
         }
         if (ct > 1000000) {
             assert(false);
@@ -24,6 +36,9 @@ void vcoProfiler(bool highToLow, oscillator osc)
         }
         ++ct;
         last = x;
+
+        sum += x;
+        ++samples;
     }
 }
 
@@ -31,13 +46,13 @@ static void testDC0()
 {
     float phase = 0;
     oscillator osc = [&phase]() {
-        phase += .1;
+        phase += .01;
         if (phase > 1) {
             phase -= 1;
         }
-        return phase;
+        return phase - .5f;
     };
-    vcoProfiler(true, osc);
+    vcoProfiler(true, 41000, osc);
 }
 
 void testDC()
