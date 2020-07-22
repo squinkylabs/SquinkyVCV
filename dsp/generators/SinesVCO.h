dc10c3624327812bbb217231b45caa25aae2df04
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2020-07-21 21:47:18 -0700

    -

--- dsp/generators/SinesVCO.h
+++ dsp/generators/SinesVCO.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include "simd.h"
+#include "SimdBlocks.h"
+//#include "dsp/minblep.hpp"
+#include "dsp/approx.hpp"
+//#include "dsp/filter.hpp"
+
+using namespace rack;		// normally I don't like "using", but this is third party code...
+
 template <typename T>
 class SinesVCO
 {
@@ -13,3 +21,24 @@ private:
     T phase = 0;
     T freq = 0;
 };
+
+template <typename T>
+inline void SinesVCO<T>::setPitch(T pitch)
+{
+	freq = dsp::FREQ_C4 * dsp::approxExp2_taylor5(pitch + 30) / 1073741824;
+    printf("set pitch %s made freq %s\n", toStr(pitch).c_str(), toStr(freq).c_str());
+}
+
+ static float_4 twoPi = 2 * 3.141592653589793238;
+
+template <typename T>
+inline T SinesVCO<T>::process(T deltaT)
+{
+    printf("in process, freq=%s\n", toStr(freq).c_str());
+  //  T deltaPhase = simd::clamp(freq * deltaT, 1e-6f, 0.35f);
+    const T deltaPhase = freq * deltaT;
+    phase += deltaPhase;
+     printf("in process, phase=%s\n", toStr(phase).c_str());
+    phase = SimdBlocks::ifelse( (phase > 1), (phase - 1), phase);
+    return SimdBlocks::sinTwoPi(phase * twoPi);
+}
