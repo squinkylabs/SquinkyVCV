8fa4a66390486a743095d9b27bff5605f715bea5
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2020-07-22 22:43:49 -0700

    -

--- composites/Sines.h
+++ composites/Sines.h
@@ -1,9 +1,13 @@
 
 #pragma once
 
+#include "Divider.h"
+#include "IComposite.h"
+#include "PitchUtils.h"
+#include "SinesVCO.h"
+
 #include <assert.h>
 #include <memory>
-#include "IComposite.h"
 
 namespace rack {
     namespace engine {
@@ -25,6 +29,7 @@ template <class TBase>
 class Sines : public TBase
 {
 public:
+    using T = float_4;
 
     Sines(Module * module) : TBase(module)
     {
@@ -49,11 +54,14 @@ public:
 
     enum InputIds
     {
+        V_OCT_INPUT,
+        GATE_INPUT,
         NUM_INPUTS
     };
 
     enum OutputIds
     {
+        MAIN_OUTPUT,
         NUM_OUTPUTS
     };
 
@@ -77,17 +85,49 @@ public:
 
 private:
 
+    SinesVCO<T> sines[3];
+    
+    Divider divn;
+
+    void stepn();
+
 };
 
 
 template <class TBase>
 inline void Sines<TBase>::init()
 {
+    divn.setup(4, [this]() {
+        this->stepn();
+    });
+}
+
+template <class TBase>
+inline void Sines<TBase>::stepn()
+{
+    const float semi = PitchUtils::semitone;
+    float pitches[12] = {
+        -1, 0, 7 * semi, 1,
+        1 + 7 * semi, 2, 2 + 4 * semi, 2 + 7 * semi,
+        3, 0, 0, 0};
+    
+    float_4 pitch;
+    pitch.load(pitches);
+    sines[0].setPitch(pitch);
+    pitch.load(pitches + 4);
+    sines[1].setPitch(pitch);
+    pitch.load(pitches + 8);
+    sines[2].setPitch(pitch);
 }
 
 template <class TBase>
 inline void Sines<TBase>::process(const typename TBase::ProcessArgs& args)
 {
+    T sum;
+    T deltaT(args.sampleTime);
+    sum = sines[0].process(deltaT);
+    sum += sines[1].process(args.sampleTime);
+    sum += sines[2].process(args.sampleTime);
 }
 
 template <class TBase>
