2ec047591f4fbcf1f82380cf63652fd6f30483d7
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2020-09-20 10:13:52 -0700

    -

--- dsp/utils/ClockRecovery.h
+++ dsp/utils/ClockRecovery.h
@@ -2,5 +2,38 @@
 
 class ClockRecovery
 {
+public:
+    /**
+     * process one input sample.
+     * return true if new period/
+     */
+    bool step(float);
 
+    /**
+     * Just for debugging
+     */
+    int _getResetCount() const;  
+    float _getFrequency()const; 
+    float _getEstimatedFrequency() const;    
+private:
 };
+
+inline bool ClockRecovery::step(float)
+{
+    return false;
+}
+
+inline int ClockRecovery::_getResetCount() const 
+{
+    return 0;
+}
+
+inline float ClockRecovery::_getFrequency() const
+{
+    return 0;
+}
+
+inline float ClockRecovery::_getEstimatedFrequency() const
+{
+    return 0;
+} 
--- test/testClockRecovery.cpp
+++ test/testClockRecovery.cpp
@@ -2,12 +2,20 @@
 #include "ClockRecovery.h"
 #include "asserts.h"
 
-static void testClockRecovery0()
+// test initial conditions
+static void testClockRecoveryInit()
 {
     ClockRecovery c;
+    assertEQ(c._getResetCount(), 0);
+    assertEQ(c._getResetCount(), 0);        // not acquired yet
+    assertEQ(c._getEstimatedFrequency(), 0);
+
+    bool b = c.step(0);
+    assert(!b);
+    assertEQ(c._getResetCount(), 0);
 }
 
 void testClockRecovery() 
 {
-    testClockRecovery0();
+    testClockRecoveryInit();
 }
