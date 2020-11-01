ae5f3dff96b4daacfaa42958447538e1a43874e8
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2020-11-01 15:00:37 -0800

    -

--- src/CompressorModule.cpp
+++ src/CompressorModule.cpp
@@ -188,7 +188,7 @@ void CompressorModule::onSampleRateChange()
 struct CompressorWidget : ModuleWidget
 {
     CompressorWidget(CompressorModule *);
-    DECLARE_MANUAL("Compressor Manual", "https://github.com/squinkylabs/SquinkyVCV/blob/main/docs/compressor.md");
+    DECLARE_MANUAL("Comp Manual", "https://github.com/squinkylabs/SquinkyVCV/blob/main/docs/compressor.md");
 
 #ifdef _LAB
     Label* addLabel(const Vec& v, const char* str, const NVGcolor& color = SqHelper::COLOR_BLACK)
