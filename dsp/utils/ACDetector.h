109210a6641f9ee692e63c6715c53a16d99577af
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-01-17 22:44:37 -0800

    -

--- dsp/utils/ACDetector.h
+++ dsp/utils/ACDetector.h
@@ -1,13 +1,49 @@
 #pragma once
 
+#include <assert.h>
+
 #include "SqPort.h"
+
 class ACDetector {
 public:
-    bool step(SqInput& input, int baseChannel);
+    bool step(SqInput& input, int numChannels);
+
 private:
+    // let's turn on at 200 hz
+    const int thresholdPeriod = 44100 / 200;
+    int counter = 0;
+    bool lastValue = false;
+    bool isACInput = false;
 };
 
-inline bool ACDetector::step(SqInput& input, int baseChannel)
-{
-    return false;
+inline bool ACDetector::step(SqInput& input, int numChannels) {
+    assert(numChannels == 1);  // todo: implement poly
+    bool detector = false;
+    if (!input.isConnected()) {
+        return false;
+    }
+
+    bool inputV = input.getVoltage(0) > .5;  // todo: schmidt
+    bool change = inputV != lastValue;
+    lastValue = inputV;
+    if (!isACInput) {
+        // if we are inactive, and we see a transion,
+        // go active immediately.
+        isACInput = change;
+
+        counter = 0;
+    } else if (change) {
+        // we active, but we got a change
+        isACInput = change;
+        counter = 0;
+    } else {
+        // are active, but no input
+        counter++;
+        if (counter > thresholdPeriod) {
+            isACInput = false;
+            counter = 0;
+        }
+    }
+
+    return isACInput;
 }
--- test/testACDetector.cpp
+++ test/testACDetector.cpp
@@ -3,16 +3,58 @@
 #include "ACDetector.h"
 #include "asserts.h"
 
-void runIt(ACDetector& det, int cyclesQuiet, int cyclesActive) {
+bool runIt(ACDetector& det, SqInput& in, int cyclesQuiet, int cyclesActive, int periodActive, int cyclesQuiet2) {
+    bool ret = false;
+    while (cyclesQuiet--) {
+        in.setVoltage(0, 0);
+        det.step(in, 1);
+    }
+    int ct = 0;
+    while (cyclesActive--) {
+        if (++ct > periodActive) {
+            ct = 0;
+        }
+        bool h = ct > periodActive / 2;
+        in.setVoltage(h ? 5.f : -5.f, 0);
+        ret = det.step(in, 1);
+    }
+    while (cyclesQuiet2--) {
+        in.setVoltage(0, 0);
+        ret = det.step(in, 1);
+    }
+    return ret;
 }
 
 void testACDetector0() {
     ACDetector ac;
     SqInput in;
-    const bool b = ac.step(in, 0);
+    const bool b = ac.step(in, 1);
     assert(!b);
 }
 
+void testACDetector1() {
+    ACDetector ac;
+    SqInput in;
+    in.setChannels(0);
+    assert(!in.isConnected());
+    const bool b = runIt(ac, in, 0, 1000, 40, 0);
+    assert(!b); 
+}
+
+
+void testACDetector2() {
+    ACDetector ac;
+    SqInput in;
+    in.channels = 1;            // pretend that it's hooked up
+    in.setChannels(1);
+    assert(in.isConnected());
+    const bool b = runIt(ac, in, 0, 1000, 40, 0);
+    assert(b);
+}
+
+
 void testACDetector() {
     testACDetector0();
+    testACDetector1();
+    testACDetector2();
 }
