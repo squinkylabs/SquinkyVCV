1f71119525d448a29fe6aa4b083e7906d95c03d9
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-02-22 08:14:16 -0800

    -

--- test/testx6.cpp
+++ test/testx6.cpp
@@ -1,7 +1,38 @@
 
+#include "CompiledInstrument.h"
+#include "SInstrument.h"
+#include "SParse.h"
+#include "SamplerErrorContext.h"
 #include "asserts.h"
 
+static CompiledInstrumentPtr makeTest(const std::string& data) {
+    SInstrumentPtr inst = std::make_shared<SInstrument>();
+    auto err = SParse::go(data, inst);
+    assert(err.empty());
+
+    SamplerErrorContext errc;
+    CompiledInstrumentPtr cinst = CompiledInstrument::make(errc, inst);
+    assert(cinst);
+    return cinst;
+}
+
+
+// verify that region overrides others
+static void testRegionAmpeg() {
+    const char* data = R"foo(<global>ampeg_release=1
+        <group>ampeg_release=2
+        <region>ampeg_release=3
+         )foo";
+
+    auto inst = makeTest(data);
+
+    std::vector<CompiledRegionPtr> regions;
+    inst->_pool()._getAllRegions(regions);
+    assertEQ(regions.size(), 1);
+    CompiledRegionPtr creg = regions[0];
+    assertEQ(creg->ampeg_release, 3);
+}
 
 void testx6() {
-    assert(false);
+    testRegionAmpeg();
 }
