1542c4a96a4920b91ad308aac954658d6bd31d5b
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-02-22 08:22:17 -0800

    -

--- test/testx6.cpp
+++ test/testx6.cpp
@@ -16,7 +16,6 @@ static CompiledInstrumentPtr makeTest(const std::string& data) {
     return cinst;
 }
 
-
 // verify that region overrides others
 static void testRegionAmpeg() {
     const char* data = R"foo(<global>ampeg_release=1
@@ -33,6 +32,57 @@ static void testRegionAmpeg() {
     assertEQ(creg->ampeg_release, 3);
 }
 
+// verify that group overrides others
+static void testGroupAmpeg() {
+    const char* data = R"foo(<global>ampeg_release=1
+        <group>ampeg_release=2
+        <region>
+         )foo";
+
+    auto inst = makeTest(data);
+
+    std::vector<CompiledRegionPtr> regions;
+    inst->_pool()._getAllRegions(regions);
+    assertEQ(regions.size(), 1);
+    CompiledRegionPtr creg = regions[0];
+    assertEQ(creg->ampeg_release, 2);
+}
+
+// verify that global works
+static void testGlobalAmpeg() {
+    const char* data = R"foo(<global>ampeg_release=1
+        <group>
+        <region>
+         )foo";
+
+    auto inst = makeTest(data);
+
+    std::vector<CompiledRegionPtr> regions;
+    inst->_pool()._getAllRegions(regions);
+    assertEQ(regions.size(), 1);
+    CompiledRegionPtr creg = regions[0];
+    assertEQ(creg->ampeg_release, 1);
+}
+
+// tests 1 ms default of ampeg_release
+static void testDefaultAmpeg() {
+    const char* data = R"foo(<global>
+        <group>
+        <region>
+         )foo";
+
+    auto inst = makeTest(data);
+
+    std::vector<CompiledRegionPtr> regions;
+    inst->_pool()._getAllRegions(regions);
+    assertEQ(regions.size(), 1);
+    CompiledRegionPtr creg = regions[0];
+    assertClosePct(creg->ampeg_release, .001f, 1);
+}
+
 void testx6() {
     testRegionAmpeg();
+    testDefaultAmpeg();
+    testGroupAmpeg();
+    testGlobalAmpeg();
 }
