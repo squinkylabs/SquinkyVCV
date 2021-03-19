7e26cece9beb2a73e9f0ec6c65260ae2842fde2c
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-03-19 08:55:09 -0700

    -

--- dsp/samp/FlacReader.cpp
+++ dsp/samp/FlacReader.cpp
@@ -1,5 +1,12 @@
 
 #include "FlacReader.h"
+#include "SqLog.h"
+//#include "stream_decoder.h"
 
 void FlacReader::read(const char* filePath) {
+	if ((decoder = FLAC__stream_decoder_new()) == NULL) {
+		SQWARN("ERROR: allocating flac decoder");
+		fclose(fout);
+		return;
+	}
 }
--- dsp/samp/FlacReader.h
+++ dsp/samp/FlacReader.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "stream_decoder.h"
+#include <stdio.h>
+
+
 class FlacReader {
 public:
     void read(const char* filePath);
 private:
+
+	FLAC__StreamDecoder *decoder = nullptr;
+//	FLAC__StreamDecoderInitStatus init_status;
+	FILE *fout = nullptr;
 
 };
